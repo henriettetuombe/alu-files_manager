4b0bcbd872be39c5ee06a0f3cf3cde8bda1fd15d
Henriette Tuombe <116841112+henriettetuombe@users.noreply.github.com>
2024-11-18 13:05:30 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,15 +1,14 @@
-import express from 'express';
-import controllerRouting from './routes/index';
+const express = require('express');
 
 const app = express();
 const port = process.env.PORT || 5000;
+const routes = require('./routes/index');
 
-app.use(express.json());
+app.use(express.json({ limit: '20mb' }));
+app.use(express.urlencoded({ extended: false, limit: '20mb' }));
 
-controllerRouting(app);
+app.use('/', routes);
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
-
-export default app;
